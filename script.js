2323159e9ac2f73e3bb21096458e046cf565e584
sanjaykumar-me <sanjaykumar12.hhhhjj@gmail.com>
2025-04-20 16:51:11 +0500

    [3

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("btn");
+const result = document.getElementsByClassName("result");
+const inpul = document.getElementById("input");
+
+function calculateAge(){
+    const birthdayValue = inpul.value;
+    if (birthdayValue === " ") {
+        alert("Enter age ");
+    }else{
+        
+    }
+
+}
